12be953eb8df67f87d3bfaa9048ab7f874273bf4
udgar <udgar.khanal@clusus.com>
2026-01-08 13:31:03 +0545

    introducing web client in order to send request from application to file system

--- application/src/main/java/com/udgar/reactorapplication/controller/FileImportController.java
+++ application/src/main/java/com/udgar/reactorapplication/controller/FileImportController.java
@@ -1,12 +1,16 @@
 package com.udgar.reactorapplication.controller;
 
 import com.udgar.reactorapplication.service.FileImportService;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_PDF_VALUE;
 
 @RestController
 @RequestMapping("/api/v1/file")
@@ -18,8 +22,8 @@ public class FileImportController {
         this.service = service;
     }
 
-    @GetMapping(value = "/import", produces = TEXT_EVENT_STREAM_VALUE)
-    private Flux<String> getFileContent() {
-        return service.fileImport();
+    @GetMapping(value = "/import", produces = APPLICATION_PDF_VALUE)
+    private ResponseEntity<Flux<DataBuffer>> getFileContent() {
+        return ResponseEntity.ok(service.fileImport());
     }
 }
--- application/src/main/java/com/udgar/reactorapplication/service/FileImportService.java
+++ application/src/main/java/com/udgar/reactorapplication/service/FileImportService.java
@@ -1,12 +1,29 @@
 package com.udgar.reactorapplication.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 @Service
 public class FileImportService {
 
-    public Flux<String> fileImport(){
-        return Flux.just("This", "is","Sparta");
+    @Value("${filesystem.file-import.url}")
+    private String fileSystemUrl;
+
+    private final WebClient.Builder builder;
+
+    public FileImportService(WebClient.Builder builder) {
+        this.builder = builder;
+    }
+
+    public Flux<DataBuffer> fileImport() {
+        WebClient client = builder.baseUrl(fileSystemUrl)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE)
+                .build();
+        return client.get().accept(MediaType.APPLICATION_PDF).exchangeToFlux(response -> response.bodyToFlux(DataBuffer.class));
     }
 }
