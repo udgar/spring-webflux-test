15f9b46ac0ea153e6db0fafbf49679d53b607fc6
udgar <udgar.khanal@clusus.com>
2026-01-08 12:34:14 +0545

    PDF downloadable from filesystemapplication

--- filesystem/src/main/java/com/udgar/reactorfilesystem/controller/FileController.java
+++ filesystem/src/main/java/com/udgar/reactorfilesystem/controller/FileController.java
@@ -1,10 +1,15 @@
 package com.udgar.reactorfilesystem.controller;
 
 import com.udgar.reactorfilesystem.service.FileGetService;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/v1/file-import")
@@ -16,8 +21,8 @@ public class FileController {
         this.service = service;
     }
 
-    @GetMapping("/import")
-    public Flux<String> getFile() {
-        return service.getFile();
+    @GetMapping(value = "/import",produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<Flux<DataBuffer>> getFile() {
+        return ResponseEntity.ok(service.getFileFormPath());
     }
 }
--- filesystem/src/main/java/com/udgar/reactorfilesystem/service/FileGetService.java
+++ filesystem/src/main/java/com/udgar/reactorfilesystem/service/FileGetService.java
@@ -1,12 +1,18 @@
 package com.udgar.reactorfilesystem.service;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 @Service
 public class FileGetService {
 
-    public Flux<String> getFile(){
-        return Flux.just("I", "am", "sparta");
+    public Flux<DataBuffer> getFileFormPath() {
+        Resource resource = new ClassPathResource("data/Report.pdf");
+        return DataBufferUtils.read(resource, new DefaultDataBufferFactory(), 4096);
     }
 }
